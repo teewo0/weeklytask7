db0792dababd76ba26d82c3fc94cc5fa44642024
teewo0 <teewodavie@gmail.com>
2021-07-21 17:44:08 -0400

    weeklytask7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import UserCard from './UserCard';
 
 function App() {
+  const user = [ 
+    {
+      name: 'Elon musk',
+      location: 'Las Vegas',
+      foodType: 'Everything',
+      age: 28,
+      likes: 'Coding into the wee hours of the morning',
+      twitterUsername: 'Elon Musk',
+      avatar: 'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'
+    },
+    {
+      name: 'Chris Morgan',
+      location: 'Florida',
+      foodType: 'Chiken and Chips',
+      age: 45,
+      likes: 'Swimming and Reading',
+      twitterUsername: 'Morgan45',
+      avatar: 'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'
+    
+    }
+    ]
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <UserCard myUser={user}/>
+      
+
+
     </div>
   );
 }
